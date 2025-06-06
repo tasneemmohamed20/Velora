f6a0ae829b98d1d413d9013de1df59d5e9967ce4
suhailafarahat <suhailafarahat8@gmail.com>
2025-06-06 23:16:33 +0300

    Design UI for Sign In / Sign Up (email/password and social login) and Start screen

---
+++ app/src/main/java/com/example/m_commerce/Routes.kt
@@ -0,0 +1,7 @@
+package com.example.m_commerce
+
+enum class Routes (val route: String){
+    START("start"),
+    LOGIN("login"),
+    SIGNUP("signup");
+}
---
+++ app/src/main/java/com/example/m_commerce/start/StartScreen.kt
@@ -0,0 +1,91 @@
+package com.example.m_commerce.start
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import com.example.m_commerce.R
+import com.example.m_commerce.Routes
+
+
+@Composable
+fun StartScreen(navController: NavController) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 24.dp),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Spacer(modifier = Modifier.height(120.dp))
+
+        Text("Velora", color = Color(0xFF0F6FB0), fontSize = 56.sp, fontWeight = FontWeight.Bold)
+        Text("Your Best Shop", fontSize = 14.sp, color = Color.Gray)
+        Spacer(modifier = Modifier.height(12.dp))
+
+        Image(
+            painter = painterResource(id = R.drawable.start_img),
+            contentDescription = "start image",
+            modifier = Modifier
+                .size(300.dp),
+            contentScale = ContentScale.Fit
+        )
+        Spacer(modifier = Modifier.height(12.dp))
+
+        Column(
+            verticalArrangement = Arrangement.spacedBy(12.dp),
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            SocialButton("Continue as Guest", icon = R.drawable.ic_person) {}
+            SocialButton("Continue with Email", icon = R.drawable.ic_gmail) {
+                navController.navigate(Routes.LOGIN.route)
+            }
+            SocialButton("Continue with Google", icon = R.drawable.ic_google) {}
+        }
+    }
+}
+
+
+
+@Composable
+fun SocialButton(text: String, icon: Int, onClick: () -> Unit) {
+    OutlinedButton(
+        onClick = onClick,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 4.dp)
+            .height(48.dp),
+        shape = RoundedCornerShape(8.dp),
+        border = BorderStroke(1.dp, Color.Black)
+    ) {
+        Icon(
+            painter = painterResource(id = icon),
+            contentDescription = null,
+            modifier = Modifier.size(20.dp),
+            tint = Color.Unspecified
+        )
+        Spacer(modifier = Modifier.width(12.dp))
+        Text(text, fontSize = 14.sp)
+    }
+}
