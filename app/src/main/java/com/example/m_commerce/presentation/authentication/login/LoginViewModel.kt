7bab46577bfe05a065543e75ad9632875eeeb08c
tasneemmohamed20 <tasneemmohamed20@gmail.com>
2025-06-20 08:48:18 +0300

    - Modified `CartViewModel` to use customer email instead of customer ID for fetching draft orders. - Changed the start destination in `MainActivity` from `ScreensRoute.Home` to `ScreensRoute.Start`. - Commented out draft order checking logic in `LoginViewModel`. - Added Paymob SDK dependency and configuration. - Enabled data binding in `app/build.gradle.kts`. - Updated Gradle settings for offline mode and Paymob repository.

--- app/src/main/java/com/example/m_commerce/presentation/authentication/login/LoginViewModel.kt
+++ app/src/main/java/com/example/m_commerce/presentation/authentication/login/LoginViewModel.kt
@@ -51,21 +51,21 @@ class LoginViewModel  @Inject constructor(
                             fetchShopifyCustomerId(email)
                             _loginState.value = ResponseState.Success("Welcome back! You have successfully logged in")
                             val customerId = sharedPreferencesHelper.getCustomerId()
-                            viewModelScope.launch {
-                                customerId?.let {
-                                    try {
-                                        getDraftOrder(it).let { hasExistingOrder ->
-                                            if (hasExistingOrder) {
-                                                Log.d("LoginViewModel", "Draft order already exists for customer: $customerId")
-                                            } else {
-                                                Log.d("LoginViewModel", "No existing draft order found for customer: $customerId")
-                                            }
-                                        }
-                                    } catch (e: Exception) {
-                                        Log.e("LoginViewModel", "Error checking draft order", e)
-                                    }
-                                }
-                            }
+//                            viewModelScope.launch {
+//                                customerId?.let {
+//                                    try {
+//                                        getDraftOrder(it).let { hasExistingOrder ->
+//                                            if (hasExistingOrder) {
+//                                                Log.d("LoginViewModel", "Draft order already exists for customer: $customerId")
+//                                            } else {
+//                                                Log.d("LoginViewModel", "No existing draft order found for customer: $customerId")
+//                                            }
+//                                        }
+//                                    } catch (e: Exception) {
+//                                        Log.e("LoginViewModel", "Error checking draft order", e)
+//                                    }
+//                                }
+//                            }
                         } else {
                             auth.signOut()
                             _loginState.value = ResponseState.Failure(
